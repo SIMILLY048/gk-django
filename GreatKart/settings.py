f78d903fc78249f6c59ca04da17a315097b233a6
Bhagavati <51833130+SIMILLY048@users.noreply.github.com>
2022-06-17 20:22:55 +0530

    django auth

--- GreatKart/settings.py
+++ GreatKart/settings.py
@@ -135,3 +135,17 @@ STATICFILES_DIRS = [
 #media files config
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    
+}
+
+
+# SMTP CONFIGURATION
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'internshipinterndemo@gmail.com'
+EMAIL_HOST_PASSWORD = 'Interndemo1234'
+EMAIL_USE_TLS = True
