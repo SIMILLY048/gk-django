9f28d00260cbf2b453dd3ff52b983a67865c85dc
Bhagavati <51833130+SIMILLY048@users.noreply.github.com>
2022-06-17 16:13:11 +0530

    store and carts functionality

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from distutils.archive_util import make_zipfile
 from distutils.command.upload import upload
+from tkinter import CASCADE
 
 #from os import ST_MANDLOCK
 from unicodedata import category
@@ -30,4 +31,26 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
 
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager,self).filter(variation_category='color', is_active=True)
+    def sizes(self):
+        return super(VariationManager,self).filter(variation_category='size', is_active=True)
+
+variation_category_choice = (
+    ('color','color'),
+    ('size','size'),
+)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete =models.CASCADE)
+    variation_category = models.CharField(max_length=100 , choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+
+    objects = VariationManager()
+
+    def __str__(self):
+        return self.variation_value
 
